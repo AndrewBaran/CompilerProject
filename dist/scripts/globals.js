ec32ea03ea88bf2d11122ac1441485abd3fdebba
AndrewBaran <andrewb.c4@gmail.com>
2015-01-26 14:11:29 -0500

    Updated and added proper tests

--- dist/scripts/globals.js
+++ dist/scripts/globals.js
@@ -7,7 +7,9 @@ function onDocumentLoad() {
 
 // Used to dynamically set up the test code in the form of buttons
 var _testCodeList = [
-    "int a\na = 10",
-    "int b\nb = 20",
-    "int c\nc = 30"
+    "{\n\tint a\n\ta = 10\n\tprint(a)\n} $",
+    "{\n\tstring s\n\ts = \"This is a string\"\n\tprint(s)\n} $",
+    "{\n\tint a\n\ta = 40\n\n\tint b\n\tb = 2\n\n\tint c\n\tc = a + b\n\tprint(c)\n} $",
+    "{\n\tint i\n\ti = 0\n\tprint(i)\n\n\tif (i == 0) {\n\t\ti = i + 1\n\t}\n\n\tprint(i)\n} $",
+    "{\n\tif true {\n\t\tprint(\"This will print\")\n\tif false {\n\t\tprint(\"This will not print\")\n\t}\n} $"
 ];
